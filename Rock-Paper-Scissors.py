e527b9706584206fe903245e59c5fec334cefb47
Apoorva-S-Natesh <104731101+Apoorva-S-Natesh@users.noreply.github.com>
2022-05-05 16:21:53 +0530

    Removed an out of range IndexError bug
    Since the user choice was checked after printing the gif, if the user gave a number >2 or <0 the gif cannot be printed since there would be an IndexError. So the condition to check the user option should be before printing the user gif

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -34,6 +34,8 @@ scissors = '''
 game_gif=[rock,paper,scissors]
 #converting player choice to integer.
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for Scissors: "))
+if player_choice >= 3 or player_choice < 0: 
+  print("You typed an invalid number, you lose!") 
 print(game_gif[player_choice])
 #randint() function is used to randomise the computer choice.
 computer_choice=random.randint(0,2)
@@ -41,8 +43,7 @@ computer_choice=random.randint(0,2)
 print("Computer Choice " + game_gif[computer_choice])
 
 #conditions which needs to be satisfied for winning or losing
-if player_choice >= 3 or player_choice < 0: 
-  print("You typed an invalid number, you lose!") 
+
 elif player_choice == 0 and computer_choice == 2:
   print("You win!")
 elif computer_choice == 0 and player_choice == 2:
